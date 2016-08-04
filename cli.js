6119ef58139bee2f31c600d16b729b77829513f2
quanwei <quanwei9958@126.com>
2016-08-04 13:58:21 +0800

    Added template file for inno

--- cli.js
+++ cli.js
@@ -10,6 +10,7 @@ function print_usag_and_exit() {
   console.log('\t--help:\n\t\tprint this message and exit');
   console.log('\t--path path:\n\t\tset work directory');
   console.log('\t--platforms plats\n\t\tset prebuild platforms,like\'s win32-x64,darwin-x64');
+  console.log('\t--make-iss make template file for inno iss');
   process.exit(1);
 }
 
@@ -46,6 +47,11 @@ while (argv.length) {
       argv.shift();
       error(argv.length===0,'require icon');
       options['icon']=(argv[0].startsWith('/')? argv[0] : path.resolve(process.cwd(),argv[0]));
+      break;
+    case '--make-iss':
+      spawn('cp',[path.resolve(path.dirname(__filename),'example.iss'),process.cwd()],{stdio:'inherit' });
+      console.log('success: output ./example.iss');
+      process.exit(0);
       break;
     default:
       options['issfile'] = argv[0];
