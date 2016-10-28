52e159e205875c964f0ba9bad7284216cad468e3
quanwei <quanwei9958@126.com>
2016-10-28 16:48:08 +0800

    refactor

--- npm-files.js
+++ npm-files.js
@@ -1,34 +1,52 @@
+"use strict"
+const R = require('ramda');
+const path = require('path');
+const fs = require('fs');
+
+/**
+* Load a file 
+* @param {} filename: string
+* @returns {} 
+*/
+function LoadJsonFile(filename){
+  return fs.existsSync(filename) ? JSON.parse(fs.readFileSync(filename)) : {};
+}
+
+/**
+* recursion find dependencies path
+* @param {} pkgPath: string
+* @returns {} 
+*/
+function Deps(pkgPath){
+  const deps = R.keys(R.pathOr({},['dependencies'])(LoadJsonFile(path.resolve(pkgPath,"package.json"))));
+  const deep = R.length(R.split('/',pkgPath));
+  const findPackage = (baseDir,pkgName) => fs.existsSync(path.resolve(baseDir,"node_modules",pkgName));
+
+  return Promise.all(R.map(function(dep){
+    return new Promise(function(resolve,reject){
+      let done = false;
+      let p = null;
+      for(let i of R.range(0,deep)){
+        p = path.resolve(pkgPath+'/../'.repeat(i),'node_modules',dep);
+        if(fs.existsSync(p)){
+          Deps(path.resolve(p)).then(function(ary){
+            resolve(R.concat([p],ary));
+          });
+          done = true;
+          break;
+        }
+      }
+      done ? null : reject('can\'t find pkg:'+dep);
+    });
+  },deps)).then(function(argv){
+    return Promise.resolve(R.compose(R.uniq,R.flatten)(argv));
+  }).catch(console.error);
+}
+
 module.exports = function(name){
-	var fs = require('fs');
-	var path = require('path');
-	var _ = require('underscore');
-	
-	var loadJsonFile = function(fileName){
-		var data = fs.readFileSync(fileName);
-		return JSON.parse(data)
-	}
-	
-	var getDeps = function(pkgPath){
-		var data = loadJsonFile(path.join(pkgPath, 'package.json'));
-		var deps = Object.keys(data.dependencies || {});
-		deps.forEach(function(depName){
-			var deepDepPath = path.join(pkgPath, 'node_modules', depName);
-			if (fs.existsSync(deepDepPath)){
-				deps = deps.concat(getDeps(deepDepPath));
-			} else {
-				var depPath = path.join('node_modules', depName);
-				if (fs.existsSync(depPath)){
-					deps = deps.concat([depName]).concat(getDeps(depPath));
-				}
-			}
-		})
-		return _.uniq(deps);
-	}
-	
-	if (name === undefined){
-		name = '.';
-	}
-	return getDeps(name).map(function(v){
-		return './node_modules/'+v+'/**/*'
-	})
+  return new Promise(function(resolve){
+    Deps(name || '.').then(function(ary){
+      resolve(R.map(v=>v+"/**/*",ary));
+    });
+  });
 }
